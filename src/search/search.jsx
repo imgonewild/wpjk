4fc1268ea2ff2e19b2ca99578fbf1800f33d8ff7
unknown <kaisite2004@gmail.com>
2023-09-28 15:29:13 -0500

    add info column and modify th name

--- src/search/search.jsx
+++ src/search/search.jsx
@@ -8,22 +8,22 @@ function Search() {
     const [data, setData] = useState([]);
     const [img, setImg] = useState(null);
     const inputRef = useRef();
-    const qrbox = window.screen.width -5
+    const qrbox = window.screen.width - 5
     let canTrigger = true;
     const onNewScanResult = (decodedText, decodedResult) => {
         if (canTrigger) {
-          canTrigger = false;
-      
-          const val = decodedResult.decodedText;
-          inputRef.current.value = val;
-          setLabel(val);
-          btn_search(val);
-      
-          setTimeout(() => {
-            canTrigger = true;
-          }, 1000); // Allow triggering again after 1 second
+            canTrigger = false;
+
+            const val = decodedResult.decodedText;
+            inputRef.current.value = val;
+            setLabel(val);
+            btn_search(val);
+
+            setTimeout(() => {
+                canTrigger = true;
+            }, 1000); // Allow triggering again after 1 second
         }
-    };      
+    };
 
     const protocol = "http:";
     const hostname = window.location.hostname;
@@ -62,8 +62,8 @@ function Search() {
                 alert('Upload failed! ' + error)
             });
     };
-    
-    const btn_search = (val)=>{
+
+    const btn_search = (val) => {
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
         setImg(val)
@@ -97,8 +97,11 @@ function Search() {
 
     return (
         <div style={{ marginTop: "15px", marginLeft: "10px", marginRight: "10px" }}>
-            <img src="./inteplast_wpjk.jpg" alt="Inteplast" height={70} style={{ marginBottom: "10px" }}/>
+            <img src="./inteplast_wpjk.jpg" alt="Inteplast" height={70} style={{ marginBottom: "10px" }} />
             <input accept=".csv" type="file" onChange={handleFileChange} />
+            {/* <button htmlFor="files">Choose File</button>
+            <input accept=".csv" type="file" style={{ visibility: "hidden" }} onChange={handleFileChange} /> */}
+
             {/* <div style={{ display: 'flex', alignItems: 'center' }}>
                 
                 
@@ -107,34 +110,54 @@ function Search() {
             <br />
             <input ref={inputRef} type="text" placeholder="Enter label" value={label} onChange={handleInputChange} onKeyDown={handleKeyDown}
             />
-            <button onClick={() =>btn_search(label)}>Search</button>
+            <button onClick={() => btn_search(label)}>Search</button>
 
             <Html5QrcodePlugin
-                    fps={20}
-                    // qrbox={qrbox}
-                    disableFlip={false}
-                    qrCodeSuccessCallback={onNewScanResult}
-                    supportedScanTypes={[Html5QrcodeScanType.SCAN_TYPE_CAMERA]}
-                />
+                fps={20}
+                // qrbox={qrbox}
+                disableFlip={false}
+                qrCodeSuccessCallback={onNewScanResult}
+                supportedScanTypes={[Html5QrcodeScanType.SCAN_TYPE_CAMERA]}
+            />
 
             {data && Object.keys(data).length > 0 && (
                 <table style={{ marginTop: "10px" }}>
                     <thead>
                         <tr>
-                            <th>Key</th>
-                            <th>Value</th>
+                            <th>Attribute</th>
+                            <th>Detail</th>
                         </tr>
                     </thead>
                     <tbody>
                         {Object.keys(data).map((key, i) => (
-                            <tr key={i}>
-                                {key === "service_id_or_serial_number" ? (
-                                    <td>service_id/serial_number</td>
+                            <React.Fragment key={i}>
+                                {key === "network_info" ? (
+                                    <tr>
+                                        <td colSpan="2" style={{ textAlign: "center", backgroundColor: "Yellow" }}>Network Info</td>
+                                    </tr>
+                                ) : key === "pc_info" ? (
+                                    <tr>
+                                        <td colSpan="2" style={{ textAlign: "center", backgroundColor: "Yellow" }}>PC Info</td>
+                                    </tr>
+                                ) : key === "os_info" ? (
+                                    <tr>
+                                        <td colSpan="2" style={{ textAlign: "center", backgroundColor: "Yellow" }}>OS Info</td>
+                                    </tr>
+                                ) : key === "software_info" ? (
+                                    <tr>
+                                        <td colSpan="2" style={{ textAlign: "center", backgroundColor: "Yellow" }}>Software Info</td>
+                                    </tr>
                                 ) : (
-                                    <td>{key}</td>
+                                    <tr>
+                                        {key === "service_id_or_serial_number" ? (
+                                            <td>Service id/serial number</td>
+                                        ) : (
+                                            <td>{key.charAt(0).toUpperCase() + key.slice(1).replaceAll("_", " ")}</td>
+                                        )}
+                                        <td>{data[key]}</td>
+                                    </tr>
                                 )}
-                                <td>{data[key]}</td>
-                            </tr>
+                            </React.Fragment>
                         ))}
                     </tbody>
                 </table>
